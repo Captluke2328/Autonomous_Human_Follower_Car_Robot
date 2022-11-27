4462145996e00ff84ca0583b2e084eb01cc88c87
Captluke2328 <lukasjohnny2328@gmail.com>
2022-11-27 09:36:16 +0800

    + PID

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from csi_camera import CSI_Camera
 from detect import *
 from camera import *
 from track import *
+import cv2
 import os
 
 os.system ('sudo systemctl restart nvargus-daemon')
+
+pError   = 0
+pid      = [0.5,0.4]
 
 if __name__ == "__main__":
     cam = Camera()
@@ -12,8 +16,8 @@ if __name__ == "__main__":
     while True:
         img,info = det.captureimage()
         
-        det.track.trackobject(info)
-        
+        det.track.trackobject(info,pid,pError)
+
         det.track.visualise(img)
         
         cv2.imshow("Capture",img)
